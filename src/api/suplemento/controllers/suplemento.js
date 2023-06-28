d1f92cd6da46a6f9429f485f59092f60e8f623f0
Andrewsjjz <andresjimenez884@gmail.com>
2023-06-28 16:46:09 -0400

    VERSION 1

---
+++ src/api/suplemento/controllers/suplemento.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * suplemento controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::suplemento.suplemento');
---
+++ src/api/suplemento/routes/suplemento.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * suplemento router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::suplemento.suplemento');
